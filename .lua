c31760c8cee6831483bfe65950df240b29ec595e
LALOL <89741263+Its-LALOL@users.noreply.github.com>
2022-11-11 22:53:32 +0300

    Update .lua

--- .lua
+++ .lua
@@ -1,2 +1,3 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Its-LALOL/LALOL-Hub/main/Universal/.lua'))()
+wait(1.5)
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Its-LALOL/LALOL-Hub/main/'..game.PlaceId..'/.lua'))()
