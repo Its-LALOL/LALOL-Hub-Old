1613097141942e63f6f053e98e685e214859a655
LALOL <89741263+Its-LALOL@users.noreply.github.com>
2022-11-11 23:57:02 +0300

    Update .lua

--- .lua
+++ .lua
@@ -1,1 +1,7 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Its-LALOL/LALOL-Hub/main/Universal/.lua')..game:HttpGet('https://raw.githubusercontent.com/Its-LALOL/LALOL-Hub/main/'..tostring(game.PlaceId)..'/.lua'))()
+local a=''
+for i,v in {10972284553} do
+	if game.PlaceId==v then
+		a=game:HttpGet('https://raw.githubusercontent.com/Its-LALOL/LALOL-Hub/main/'..tostring(game.PlaceId)..'/.lua')
+	end
+end
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Its-LALOL/LALOL-Hub/main/Universal/.lua')..a)()
