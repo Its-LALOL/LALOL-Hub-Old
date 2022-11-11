8a571ddc02173415c7257c461fd9577705da2786
LALOL <89741263+Its-LALOL@users.noreply.github.com>
2022-11-11 22:36:11 +0300

    Update .lua

--- 10972284553/.lua
+++ 10972284553/.lua
@@ -1,1 +1,50 @@
-1
+autoclicker=false
+autoupgrade=false
+
+local upgrade_args = {
+    [1] = game:GetService("Players").LocalPlayer.PlayerGui.GameGui.Upgrade
+}
+
+local function click()
+	if autoclicker then
+		game:GetService('ReplicatedStorage').Click:FireServer()
+	end
+end
+local function upgrade()
+	if autoupgrade then
+		game:GetService("ReplicatedStorage").ClickUpgrade:InvokeServer(unpack(upgrade_args)) -- Click Upgrade
+		for i=1,3 do -- Turtles
+			game:GetService("ReplicatedStorage").IncomeEvent:FireServer(unpack({[1] = i, [2] = 0}))
+		end
+	end
+end
+
+local skala=window:NewTab('Skala Simulator')
+local autofarm=tab:NewSection('AutoFarm')
+local others=tab:NewSection('Others')
+
+skala:NewToggle('AutoClicker', 'google.com', function(state)
+	if state then
+		autoclicker=true
+	else
+		autoclicker=false
+	end
+end)
+skala:NewToggle('AutoUpgrader', 'hello!!!', function(state)
+	if state then
+		autoupgrade=true
+	else
+		autoupgrade=false
+	end
+end)
+others:NewButton('Free Badge', 'bye111111', function()
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack({[1]='42', [2]='All'}))
+end)
+
+while wait(0.1) do
+	for i=1,10 do
+		click()
+		wait(0.1)
+	end
+	upgrade()
+end
