5002de3f72d1e18517550bea122013040e6eeddf
LALOL <89741263+Its-LALOL@users.noreply.github.com>
2022-11-13 14:58:07 +0300

    Update .lua

--- .lua
+++ .lua
@@ -1,6 +1,6 @@
 game.StarterGui:SetCore('SendNotification', {Title='LALOL Hub', Text='Thx for using!', Icon='http://www.roblox.com/asset/?id=8941506186', Duration=5})
 local a=''
-for i,v in {10972284553, 10710676163} do
+for i,v in {10972284553, 10710676163, 4522347649} do
 	if game.PlaceId==v then
 		a=game:HttpGet('https://raw.githubusercontent.com/Its-LALOL/LALOL-Hub/main/'..tostring(game.PlaceId)..'/.lua')
 	end
