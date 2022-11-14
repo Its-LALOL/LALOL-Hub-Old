dbf0a9cc70d84c9c206616d12acb324f65decaa1
LALOL <89741263+Its-LALOL@users.noreply.github.com>
2022-11-14 23:05:15 +0300

    Update .lua

--- 4522347649/.lua
+++ 4522347649/.lua
@@ -2,47 +2,61 @@ local function command(text)
 	game:GetService("ReplicatedStorage").HDAdminClient.Signals.RequestCommand:InvokeServer(unpack({[1]=text}))
 end
 
-fa1=window:NewTab('Free Admin')
-fa=fa1:NewSection('Free Admin')
-fa:NewButton('Explode all', '', function()
-	for i,v in pairs(game:FindService("Players"):GetPlayers()) do
-		command(':explode '..v.name)
-	end
-end)
-fa:NewButton('Spam', '', function()
-	for i,v in pairs(game:FindService("Players"):GetPlayers()) do
-		local num=math.random(1, 3)
-		if num==1 then
-			emoji='💥'
-		elseif num==2 then
-			emoji='🎃'
-		elseif num==3 then
-			emoji='🤡'
+fa=window:CreateTab('Free Admin')
+fa:CreateButton({
+	Name='Explode all',
+	Callback=function()
+		for i,v in pairs(game:FindService("Players"):GetPlayers()) do
+			command(':explode '..v.name)
 		end
-		command(':talk '..v.name..' '..emoji..' LALOL Hub '..emoji)
-	end
-end)
-fa:NewButton('Spam 2.0', '', function()
-	for i=1,5 do
-		local num=math.random(1, 3)
-		if num==1 then
-			emoji='💥'
-		elseif num==2 then
-			emoji='🎃'
-		elseif num==3 then
-			emoji='🤡'
+	end,
+})
+fa:CreateButton({
+	Name='Spam',
+	Callback=function()
+		for i,v in pairs(game:FindService("Players"):GetPlayers()) do
+			local num=math.random(1, 3)
+			if num==1 then
+				emoji='💥'
+			elseif num==2 then
+				emoji='🎃'
+			elseif num==3 then
+				emoji='🤡'
+			end
+			command(':talk '..v.name..' '..emoji..' LALOL Hub '..emoji)
 		end
-		command(':sm '..emoji..' LALOL Hub '..emoji)
-		wait(3)
-	end
-end)
-fa:NewButton('Jail all', '', function()
-	for i,v in pairs(game:FindService("Players"):GetPlayers()) do
-		command(':jail '..v.name)
-	end
-end)
-fa:NewButton('Scary Sounds', '', function()
-	command(':music 4737106301')
-	command(':volume 9999999')
-	command(':pitch 0.3')
-end)
+	end,
+})
+fa:CreateButton({
+	Name='Spam 2.0',
+	Callback=function()
+		for i=1,5 do
+			local num=math.random(1, 3)
+			if num==1 then
+				emoji='💥'
+			elseif num==2 then
+				emoji='🎃'
+			elseif num==3 then
+				emoji='🤡'
+			end
+			command(':sm '..emoji..' LALOL Hub '..emoji)
+			wait(3)
+		end
+	end,
+})
+fa:CreateButton({
+	Name='Jail all',
+	Callback=function()
+		for i,v in pairs(game:FindService("Players"):GetPlayers()) do
+			command(':jail '..v.name)
+		end
+	end,
+})
+fa:CreateButton({
+	Name='Scary Sounds',
+	Callback=function()
+		command(':music 4737106301')
+		command(':volume 9999999')
+		command(':pitch 0.3')
+	end,
+})
