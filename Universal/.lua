92c1a9832794d82e38cd03d83aac340fabb62b44
LALOL <89741263+Its-LALOL@users.noreply.github.com>
2022-11-14 23:03:25 +0300

    Update .lua

--- Universal/.lua
+++ Universal/.lua
@@ -1,3 +1,30 @@
+local Rayfield=loadstring(game:HttpGet(('https://raw.githubusercontent.com/Its-LALOL/Rayfield/main/.lua')))()
+local window=Rayfield:CreateWindow({
+	Name='LALOL Hub',
+	LoadingTitle='LALOL Hub',
+	LoadingSubtitle='Thanks for using!',
+	ConfigurationSaving={
+		Enabled=false,
+		FolderName=nil,
+		FileName='LALOL Hub'
+	},
+        Discord={
+        	Enabled=true,
+        	Invite='XXqzxT7E5z',
+        	RememberJoins=true
+        },
+	KeySystem=true,
+	KeySettings={
+		Title='LALOL Hub',
+		Subtitle='Key System',
+		Note='discord.gg/XXqzxT7E5z',
+		FileName='LALOL Hub Key',
+		SaveKey=true,
+		GrabKeyFromSite=false,
+		Key='\48\49\56\55\53'
+	}
+})
+
 _G.walkspeed_value=game.Players.LocalPlayer.Character.Humanoid.WalkSpeed
 _G.jumppower_value=game.Players.LocalPlayer.Character.Humanoid.JumpPower
 _G.gravity_value=game.Workspace.Gravity
@@ -21,20 +48,44 @@ spawn(walkspeed)
 spawn(jumppower)
 spawn(gravity)
 
-local libary=loadstring(game:HttpGet('https://raw.githubusercontent.com/Its-LALOL/Kavo-UI-Library/main/.lua'))()
-local window=libary.CreateLib('\76\65\76\79\76\32\72\117\98\32\124\32\100\105\115\99\111\114\100\46\103\103\47\88\88\113\122\120\84\55\69\53\122', 'Ocean')
-local universal=window:NewTab('Universal')
-
-local player=universal:NewSection('Player')
-player:NewSlider('Walk Speed', '', 100, 16, function(i)
-	_G.walkspeed_value=i
-end)
-player:NewSlider('Jump Power', '', 300, 50, function(i)
-	_G.jumppower_value=i
-end)
-player:NewSlider('Gravity', '', 196, 0, function(i)
-	_G.gravity_value=i
-end)
-player:NewButton('Sit', '', function()
-	game.Players.LocalPlayer.Character.Humanoid.Sit=true
-end)
+local universal=window:CreateTab('Universal')
+local player=universal:CreateSection('Player')
+universal:CreateSlider({
+	Name='Walk Speed',
+	Range={0, 100},
+	Increment=1,
+	Suffix='Speed',
+	CurrentValue=16,
+	Flag='WalkSpeed',
+	Callback=function(i)
+		_G.walkspeed_value=i
+	end,
+})
+universal:CreateSlider({
+	Name='Jump power',
+	Range={0, 300},
+	Increment=1,
+	Suffix='Power',
+	CurrentValue=50,
+	Flag='JumpPower',
+	Callback=function(i)
+		_G.jumppower_value=i
+	end,
+})
+universal:CreateSlider({
+	Name='Gravity',
+	Range={0, 300},
+	Increment=1,
+	Suffix='',
+	CurrentValue=196,
+	Flag='Gravity',
+	Callback=function(i)
+		_G.gravity_value=i
+	end,
+})
+universal:CreateButton({
+	Name='Sit',
+	Callback=function()
+		game.Players.LocalPlayer.Character.Humanoid.Sit=true
+	end,
+})
