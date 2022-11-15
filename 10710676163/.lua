ec60a269124b926a0c852aad1acaa366f7a70d15
LALOL <89741263+Its-LALOL@users.noreply.github.com>
2022-11-15 10:56:13 +0300

    Update .lua

--- 10710676163/.lua
+++ 10710676163/.lua
@@ -14,51 +14,86 @@ local function remote(what)
 	}
 	game:GetService("ReplicatedStorage").RemoteEvent:FireServer(unpack(args))
 end
-local spts=window:NewTab('SPTS')
-local autofarm=spts:NewSection('AutoFarm')
-autofarm:NewSlider('Level (BETA)', '', 5, 1, function(i)
-	level=i
-end)
-autofarm:NewToggle('Fist Strength', '', function(state)
-	if state then
-		fist_strength=true
-	else
-		fist_strength=false
-	end
-end)
-autofarm:NewToggle('Body Tougness', '', function(state)
-	if state then
-		body_tougness=true
-	else
-		body_tougness=false
-	end
-end)
-autofarm:NewToggle('Movement Speed', '', function(state)
-	if state then
-		movement_speed=true
-	else
-		movement_speed=false
-	end
-end)
-autofarm:NewToggle('Jump Force', '', function(state)
-	if state then
-		jump_force=true
-	else
-		jump_force=false
-	end
-end)
-autofarm:NewToggle('Psychic Power', '', function(state)
-	if state then
-		psychic_power=true
-	else
-		psychic_power=false
-	end
-end)
-
-local others=spts:NewSection('Others')
-others:NewButton('Safe Zone++', '', function()
-	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame=CFrame.new(-3949.56323, 99999999999999999, -3439.6062, -0.98901552, 0.00691204006, -0.147650108, 9.99347094e-09, 0.998906016, 0.0467623696, 0.1478118, 0.0462487079, -0.987933576)
-end)
+local spts=window:CreateTab('SPTS')
+local autofarm=spts:CreateSection('AutoFarm')
+spts:CreateSlider({
+	Name='Level (BETA)',
+	Range={1, 5},
+	Increment=1,
+	Suffix='',
+	CurrentValue=1,
+	Flag='Level',
+	Callback=function(i)
+		level=i
+	end,
+})
+spts:CreateToggle({
+	Name='Fist Strength',
+	CurrentValue=false,
+	Flag='Fist_Strength',
+	Callback=function(state)
+		if state then
+			fist_strength=true
+		else
+			fist_strength=false
+		end
+	end,
+})
+spts:CreateToggle({
+	Name='Body Tougness',
+	CurrentValue=false,
+	Flag='Body_Tougness',
+	Callback=function(state)
+		if state then
+			body_tougness=true
+		else
+			body_tougness=false
+		end
+	end,
+})
+spts:CreateToggle({
+	Name='Movement Speed',
+	CurrentValue=false,
+	Flag='Movement_Speed',
+	Callback=function(state)
+		if state then
+			movement_speed=true
+		else
+			movement_speed=false
+		end
+	end,
+})
+spts:CreateToggle({
+	Name='Jump Force',
+	CurrentValue=false,
+	Flag='Jump_Force',
+	Callback=function(state)
+		if state then
+			jump_force=true
+		else
+			jump_force=false
+		end
+	end,
+})
+spts:CreateToggle({
+	Name='Psychic Power',
+	CurrentValue=false,
+	Flag='Psychic_Power',
+	Callback=function(state)
+		if state then
+			psychic_power=true
+		else
+			psychic_power=false
+		end
+	end,
+})
+local others=spts:CreateSection('Others')
+spts:CreateButton({
+	Name='Safe Zone++',
+	Callback=function()
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame=CFrame.new(-3949.56323, 99999999999999999, -3439.6062, -0.98901552, 0.00691204006, -0.147650108, 9.99347094e-09, 0.998906016, 0.0467623696, 0.1478118, 0.0462487079, -0.987933576)
+	end,
+})
 
 while wait(0.1) do
 	if fist_strength then
