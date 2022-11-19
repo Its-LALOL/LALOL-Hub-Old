6bce6ab746fe68b23ddd03ee02494d3f9e263694
LALOL <89741263+Its-LALOL@users.noreply.github.com>
2022-11-19 19:21:47 +0300

    Update .lua

--- 4522347649/.lua
+++ 4522347649/.lua
@@ -53,6 +53,18 @@ fa:CreateButton({
 	end,
 })
 fa:CreateButton({
+	Name='Crash all',
+	Callback=function()
+		old=player.Character.HumanoidRootPart.CFrame
+		player.Character.HumanoidRootPart.CFrame=CFrame.new(-3949.56323, 99999999999999999, -3439.6062, -0.98901552, 0.00691204006, -0.147650108, 9.99347094e-09, 0.998906016, 0.0467623696, 0.1478118, 0.0462487079, -0.987933576)
+		for i,v in pairs(game.Players:GetPlayers()) do
+			command(';bring '..v.name)
+		end
+		wait(3)
+		player.Character.HumanoidRootPart.CFrame=old
+	end,
+})
+fa:CreateButton({
 	Name='Control all',
 	Callback=function()
 		for i,v in pairs(game.Players:GetPlayers()) do
